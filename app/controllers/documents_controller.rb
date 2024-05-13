fd0ccc2397724443324a7ddf6feb50ef38e34c54
Roby Parr <roby.parr@gmail.com>
2024-05-13 22:25:51 +0900

    Fix error when navigating to a root-level document

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,12 +2,23 @@
 
 class DocumentsController < ApplicationController
   def index
-    @documents = current_organization.documents.where parent: nil
+    @documents = root_documents
   end
 
   def show
     @document = current_organization.documents.find params[:id]
-    @documents = @document.file? ? @document.parent.children : @document.children
-    @documents = @documents.sort_by(&:basename)
+    @documents =
+      if @document.file?
+        @document&.parent&.children || root_documents
+      else
+        @document.children
+      end
+    @documents = @documents.to_a.sort_by(&:basename)
+  end
+
+  private
+
+  def root_documents
+    current_organization.documents.where parent: nil
   end
 end
