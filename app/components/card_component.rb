6d465d993ce1956817c6f3273a023b14a4009f1a
Roby Parr <roby.parr@gmail.com>
2024-05-29 21:48:13 +0900

    Make some small dark mode tweaks

--- app/components/card_component.rb
+++ app/components/card_component.rb
@@ -1,7 +1,7 @@
 # https://flowbite.com/docs/components/card/
 class CardComponent < ApplicationComponent
   erb_template <<-ERB
-    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-slate-800 dark:border-slate-700">
       <%= content %>
     </div>
   ERB
